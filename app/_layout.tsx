39b6cf6134b245f64b6cc726985100fda8710c27
Arun-Sanjay <thearunsanjay@gmail.com>
2026-03-31 02:22:24 +0530

    Fix: Initialize all stores on startup + add missing route layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,16 @@ import { Stack, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useOnboardingStore } from "../src/stores/useOnboardingStore";
+import { useComboStore } from "../src/stores/useComboStore";
+import { useSessionStore } from "../src/stores/useSessionStore";
+import { useFightStore } from "../src/stores/useFightStore";
+import { useProfileStore } from "../src/stores/useProfileStore";
+import { useWeightStore } from "../src/stores/useWeightStore";
+import { useTimerStore } from "../src/stores/useTimerStore";
+import { useProgramStore } from "../src/stores/useProgramStore";
+import { useBenchmarkStore } from "../src/stores/useBenchmarkStore";
+import { useCampStore } from "../src/stores/useCampStore";
+import { useKnowledgeStore } from "../src/stores/useKnowledgeStore";
 
 export default function RootLayout() {
   const [ready, setReady] = useState(false);
@@ -10,7 +20,18 @@ export default function RootLayout() {
   const completed = useOnboardingStore((s) => s.completed);
 
   useEffect(() => {
+    // Load ALL stores from MMKV on app startup
     loadOnboarding();
+    useComboStore.getState().loadFromMMKV();
+    useSessionStore.getState().loadFromMMKV();
+    useFightStore.getState().loadFromMMKV();
+    useProfileStore.getState().loadFromMMKV();
+    useWeightStore.getState().loadFromMMKV();
+    useTimerStore.getState().loadFromMMKV();
+    useProgramStore.getState().loadFromMMKV();
+    useBenchmarkStore.getState().loadFromMMKV();
+    useCampStore.getState().loadFromMMKV();
+    useKnowledgeStore.getState().loadFromMMKV();
     setReady(true);
   }, [loadOnboarding]);
 
